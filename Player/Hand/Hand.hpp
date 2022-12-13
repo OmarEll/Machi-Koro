798c33a8374c94f39dca6bdf065381dd820c4b60
The Morrigan <53189749+OmarEll@users.noreply.github.com>
2022-12-13 18:21:40 +0100

    Update Hand.hpp

--- Player/Hand/Hand.hpp
+++ Player/Hand/Hand.hpp
@@ -12,7 +12,7 @@ using namespace std;
 class Hand{
 
 private:
-    map<Establishment*,size_t> establishments;
+    map<EstablishmentsNames,pair<Establishment*,size_t>> establishments;
     map<LandmarksNames,Landmark*> landmarks;
 public:
     friend class Establishment;
@@ -22,7 +22,7 @@ public:
     vector<Establishment*> getColorCards(Colors);
     vector<Establishment*> getTypeCards(Types);
     map<LandmarksNames, Landmark*> getLandmarks() const {return landmarks;}
-    map<Establishment*, size_t> getEstablishments() const {return establishments;}
+    map<EstablishmentsNames,pair<Establishment*,size_t>> getEstablishments() const {return establishments;}
     //1 methode qui permet d'acceder à tout les noms des cartes qu'il y a dans la main
     //1 methode qui permet d'acceder à tout les types des cartes qu'il y a dans la main
     //1 methode qui permet d'acceder à toute les couleurs qu'il y a dans la main
